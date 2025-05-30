358eaa9080c6ac827792da37638e53510a1ff698
Erik Diep <118822614+Nooodlz@users.noreply.github.com>
2025-05-30 18:46:42 +0200

    Delete Data/Opt_1 directory

--- Data/Opt_1/OPT_setting1/serialize_utils_setting_1.py
+++
@@ -1,9 +0,0 @@
-import pickle
-
-def serialize_to_file(obj, file_path):
-    with open(file_path, 'wb') as file:
-        pickle.dump(obj, file)
-
-def deserialize_from_file(file_path):
-    with open(file_path, 'rb') as file:
-        return pickle.load(file)
